f56194ddbcd1e7c86d947a292580921f5f6cfdbe
xxuuu <yuliyahvan@yahoo.com>
2018-12-18 14:38:12 +0600

    Add html file for update & delete functions and db.txt for information storage in dictionary

--- hello.py
+++ hello.py
@@ -1,6 +1,31 @@
+import json
+
 from flask import Flask
+from flask import render_template
+from flask import request
+
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Hello and Bye World!"
+	with open('db.txt', 'r') as f:
+		items = json.load(f)
+	if request.method == 'POST':
+		item = request.form.get('item')
+		quantity = request.form.get('quantity')
+		update = request.form.get('update')
+		delete = request.form.get('delete')
+#		for item, quantity in items.items():
+		if update: 
+			items.update({item: quantity})
+			with open('db.txt', 'w') as f2:
+				json.dump(items, f2)
+		elif delete:
+			items.pop(item)
+			with open('db.txt', 'w') as f2:
+				json.dump(items, f2)
+	return render_template('hello.html', items=items)
+
+
