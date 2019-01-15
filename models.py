c48e8c5cc77ac8a657b43fbb9e439b00ef7301ba
xxuuu <yuliyahvan@yahoo.com>
2019-01-15 23:49:11 +0600

    1: Updated models.py for Classes: Item, Customer, Cart; 2: hello.py for api; 3: templates for index root

--- models.py
+++ models.py
@@ -1,53 +1,26 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from peewee import CharField, IntegerField, Model, SqliteDatabase
-
-#create flask application
-app = Flask(__name__)
+from peewee import CharField, IntegerField, Model, SqliteDatabase, ForeignKeyField
 
 #creata a peewee database
 database = SqliteDatabase('my_app.db')
 
 #create standard baseModel
 class BaseModel(Model):
-	class Meta:
-		database = database
+    class Meta:
+        database = database
 
 #Item Model with its name and quantity as fields
 class Item(BaseModel):
     name = CharField()
     quantity = IntegerField()
 
-#function to create tables
-def create_tables():
-	with database:
-		database.create_tables([Item])
+class Customer(BaseModel):
+    name = CharField()
+    age = IntegerField()
 
-@app.before_request
-def before_request():
-    database.connect()
+class Cart(BaseModel):
+    customer = ForeignKeyField(Customer, backref='carts')
 
-@app.after_request
-def after_request(response):
-    database.close()
-    return response
+class CartItem(BaseModel):
+    cart = ForeignKeyField(Cart, backref='items')
+    item = ForeignKeyField(Item, backref='carts')
 
-@app.route('/')
-def index():
-#Return index page of the web app
-    return render_template('index.html')
-
-@app.route('/items', methods=['GET', 'POST'])
-def items():
-    if request.method == 'POST' and request.form['add']:
-    	with database.atomic():
-    		item = Item.create(
-    			name=request.form['_'],
-    			quantity=request.form[0])
-    return render_template('items.html')		
-
-
-if __name__ == '__main__':
-    create_tables()
-    app.run()
