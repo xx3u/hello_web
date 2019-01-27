d2f9dea5c8ec135fcb9a614110c5f67829520a59
xxuuu <yuliyahvan@yahoo.com>
2019-01-27 13:55:08 +0600

    Updated requirements for telegram-bot

--- hello.py
+++ hello.py
@@ -27,7 +27,6 @@ admin.add_view(UserAdmin(User))
 admin.add_view(ItemAdmin(Item))
 admin.add_view(CustomerAdmin(Customer))
 admin.add_view(CartAdmin(Cart))
-#admin.add_view(CustomerAdmin(CartItem))
 
 
 # Create a user to test with
